0730a6da784b9da44935764a104fa52243c2e0a1
welliston <welliston10@gmail.com>
2021-10-25 14:40:43 -0300

    Deleting .settings folder

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -116,7 +116,7 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL ur, ResourceBundle rb) {
 		initializeNodes();
 	}
 
